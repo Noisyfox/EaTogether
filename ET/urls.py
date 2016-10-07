361b6ee08cbd71633141923033a7aeed9e896be5
meng-PC\meng <mengyuanclaire@gmail.com>
2016-10-08 04:28:46 +1100

    ref T10

--- ET/urls.py
+++ ET/urls.py
@@ -6,4 +6,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r"^admin/", include(admin.site.urls)),
+
 ]
