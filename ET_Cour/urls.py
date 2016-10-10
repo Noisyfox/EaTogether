b6df7c031fe57dba216963284582880b634681bd
DESKTOP-L348GSO\FanZhang <fyzhang@outlook.com>
2016-10-10 16:29:11 +1100

    T9.1 Courier Frontend Page

--- ET_Cour/urls.py
+++ ET_Cour/urls.py
@@ -2,6 +2,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from ET_Cour import views
+
 
 urlpatterns = [
+    url(r"^login/$", TemplateView.as_view(template_name="ET_Cour/login.html")),
+    url(r"^detail/$", TemplateView.as_view(template_name="ET_Cour/detail.html")),
+    url(r"^order/$", TemplateView.as_view(template_name="ET_Cour/order.html")),
 ]
