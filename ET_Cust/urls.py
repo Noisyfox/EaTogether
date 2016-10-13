fbdfd1fc75a62109a091c865cdc886be2601229e
MeloSydney <shifan0525@gmail.com>
2016-10-14 03:04:30 +1100

    Ref T9: All customer related page included.

--- ET_Cust/urls.py
+++ ET_Cust/urls.py
@@ -4,5 +4,14 @@ from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    url(r"^$", TemplateView.as_view(template_name="ET_Cust/homepage.html"), name="home"),
+    url(r"^customer_login/", TemplateView.as_view(template_name="ET_Cust/Customer Login.html"), name="home"),
+    url(r"^customer_register/", TemplateView.as_view(template_name="ET_Cust/Customer Register.html"), name="home"),
+    url(r"^customer_account_order/", TemplateView.as_view(template_name="ET_Cust/Customer Account (Order).html"), name="home"),
+    url(r"^customer_account_favourite/", TemplateView.as_view(template_name="ET_Cust/Customer Account (Favourite).html"), name="home"),
+    url(r"^customer_account_profile&wallet/", TemplateView.as_view(template_name="ET_Cust/Customer Account (Profile & Wallet).html"), name="home"),
+    url(r"^customer_create_group/", TemplateView.as_view(template_name="ET_Cust/Customer Create Group.html"), name="home"),
+    url(r"^customer_main_page/", TemplateView.as_view(template_name="ET_Cust/Customer Main Page.html"), name="home"),
+    url(r"^customer_restaurant_page_group/", TemplateView.as_view(template_name="ET_Cust/Customer Restaurant Page (Group).html"), name="home"),
+    url(r"^customer_restaurant_page_menu/", TemplateView.as_view(template_name="ET_Cust/Customer Restaurant Page (Menu).html"), name="home"),
+    url(r"^customer_retrieve_password/", TemplateView.as_view(template_name="ET_Cust/Customer Retrieve Password.html"), name="home"),
 ]
