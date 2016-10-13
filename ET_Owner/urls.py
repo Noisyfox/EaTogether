f835b12fb04799f2cf2a8fd82668507d6590903f
meng-PC\meng <mengyuanclaire@gmail.com>
2016-10-14 08:54:36 +1100

    Ref:T10

--- ET_Owner/urls.py
+++ ET_Owner/urls.py
@@ -5,4 +5,8 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r"^login/$", TemplateView.as_view(template_name="ET_Owner/owner_login.html"), name="owner_login"),
+    url(r"^register/$", TemplateView.as_view(template_name="ET_Owner/owner_register.html"), name="owner_register"),
+    url(r"^editfood/$", TemplateView.as_view(template_name="ET_Owner/owner_edit_food.html"), name="owner_edit_food"),
+    url(r"^retrieve/$", TemplateView.as_view(template_name="ET_Owner/owner_password_retrieve.html"), name="owner_retrieve_password"),
+    url(r"^myrestaurant/$", TemplateView.as_view(template_name="ET_Owner/owner_my_restaurant.html.html"), name="owner_restaurant_info"),
 ]
