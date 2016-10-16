4b8bd1da3b374853ee5d124bdc18f1420bb202cd
Yulin Zhou <Yulin Zhou>
2016-10-17 01:23:57 +1100

    Accomplish parts of function in the mainpage of customers.

--- ET_Cust/urls.py
+++ ET_Cust/urls.py
@@ -1,12 +1,17 @@
-from django.conf.urls import url, include
-from django.contrib import admin
+from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from ET_Cust import views
+
+app_name = 'cust'
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="ET_Cust/homepage.html"), name="home"),
     url(r"^login/$", views.CustomerLoginView.as_view(), name='cust_login'),
     url(r"^register/$", views.CustomerRegisterView.as_view(), name='cust_register'),
-    url(r"^groupcreate/$", views.GroupCreateView.as_view(), name='group_create'),
+    url(r"^mainpage/$", views.CustomerMainPageView.as_view(), name='cust_main_page'),
+    url(r"^restaurant/(?P<restaurant_id>[0-9]+)/group",
+        TemplateView.as_view(template_name="ET_Cust/customer_restaurant_group_page.html"),
+        name="cust_restaurant_group_page")
+
 ]
