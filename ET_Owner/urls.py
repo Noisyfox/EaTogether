d1032844700988fe46ef8efe04ee8c5a193eb34f
meng-PC\meng <mengyuanclaire@gmail.com>
2016-10-19 01:59:06 +1100

    Ref:T10

--- ET_Owner/urls.py
+++ ET_Owner/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     url(r"^register/$", TemplateView.as_view(template_name="ET_Owner/owner_register.html"), name="owner_register"),
     url(r"^editfood/$", TemplateView.as_view(template_name="ET_Owner/owner_edit_food.html"), name="owner_edit_food"),
     url(r"^retrieve/$", TemplateView.as_view(template_name="ET_Owner/owner_password_retrieve.html"), name="owner_retrieve_password"),
-    url(r"^myrestaurant/$", TemplateView.as_view(template_name="ET_Owner/owner_my_restaurant.html.html"), name="owner_restaurant_info"),
+    url(r"^wallet/$", TemplateView.as_view(template_name="ET_Owner/owner_wallet.html"), name="owner_wallet_info"),
+    url(r"^delivery/$", TemplateView.as_view(template_name="ET_Owner/owner_delivery.html"), name="owner_courier_info"),
+    url(r"^menu/$", TemplateView.as_view(template_name="ET_Owner/owner_menu.html"), name="owner_menu"),
+
+    url(r"^myrestaurant/$", TemplateView.as_view(template_name="ET_Owner/owner_my_restaurant.html"), name="owner_restaurant_info"),
 ]
