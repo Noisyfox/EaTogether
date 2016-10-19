b7c4de0b2386da1a55e067588a5b4f854e7a188d
Yulin Zhou <Yulin Zhou>
2016-10-19 19:15:46 +1100

    The menu can be displayed.

--- ET_Cust/urls.py
+++ ET_Cust/urls.py
@@ -9,9 +9,13 @@ urlpatterns = [
     url(r"^register/$", views.CustomerRegisterView.as_view(), name='cust_register'),
     url(r"^search/$", views.CustomerSearchView.as_view(), name='cust_search'),
     url(r"^mainpage/$", views.CustomerMainPageView.as_view(), name='cust_main_page'),
-    url(r"^restaurant/(?P<restaurant_id>[0-9]+)/group",
-        TemplateView.as_view(template_name="ET_Cust/customer_restaurant_group_page.html"),
-        name='cust_restaurant_group_page'),
-    url(r"^creategroup/$", TemplateView.as_view(template_name="ET_Cust/customer_restaurant_menu_page.html"),
-        name='home'),
+    url(r"^restaurant/(?P<restaurant_id>[0-9]+)/group/$",
+        views.CustomerRestaurantGroupView.as_view(template_name="ET_Cust/customer_restaurant_group_page.html"),
+        name='cust_restaurant_group'),
+    url(r"^restaurant/(?P<restaurant_id>[0-9]+)/group/create/$",
+        views.CustomerCreateGroupView.as_view(template_name="ET_Cust/customer_create_group.html"),
+        name='cust_create_group'),
+    url(r"^restaurant/(?P<restaurant_id>[0-9]+)/group/(?P<group_id>[0-9]+)/$",
+        views.CustomerRestaurantMenuView.as_view(template_name="ET_cust/customer_restaurant_menu_page.html"),
+        name='cust_restaurant_menu')
 ]
