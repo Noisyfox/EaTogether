47bbc13a2cfcb0a6dd2f63c79b8df56497b28baa
noisyfox <timemanager.rick@gmail.com>
2016-10-26 00:09:02 +1100

    Add debug admin views

--- ET_Admin/urls.py
+++ ET_Admin/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
-
 urlpatterns = [
 ]
+
+if settings.DEBUG:
+    urlpatterns += [url(r'^super/', include(admin.site.urls)), ]
