af6e03d4644fc8625a3a308b9cf945a0c325416c
noisyfox <timemanager.rick@gmail.com>
2016-10-25 22:28:43 +1100

    Ref T19 Add courier order list view

--- ET_Cour/urls.py
+++ ET_Cour/urls.py
@@ -6,6 +6,10 @@ from ET_Cour import views
 
 urlpatterns = [
     url(r"^login/$", views.CourierLoginView.as_view(), name='cour_login'),
-    url(r"^detail/$", TemplateView.as_view(template_name="ET_Cour/detail.html"), name='cour_detail'),
-    url(r"^order/$", TemplateView.as_view(template_name="ET_Cour/order.html"), name='cour_order'),
+    url(r"^order/", include([
+        url(r"^$", views.CourierOrderListView.as_view(), name='cour_order'),
+        url('^(?P<order_id>[0-9]+)/', include([
+            url(r"^$", TemplateView.as_view(template_name="ET_Cour/detail.html"), name='cour_detail'),
+        ])),
+    ])),
 ]
--- ET_Cour/views.py
+++ ET_Cour/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import ListView
 
 from ET.views import LoginView
 from ET_Cour.forms import CourierLoginForm
+from ET_Cour.mixins import CourierRequiredMixin
 
 
 class CourierLoginView(LoginView):
@@ -12,3 +14,12 @@ class CourierLoginView(LoginView):
 
     def get_login_url(self):
         return reverse_lazy('cour_login')
+
+
+class CourierOrderListView(CourierRequiredMixin, ListView):
+    template_name = 'ET_Cour/order.html'
+    context_object_name = 'orders'
+    allow_empty = True
+
+    def get_queryset(self):
+        return self.request.user.courier.grouporder_set.order_by('-delivery_start_time')
