ecf08f5eef07718520164b21faa646f11a0150d9
noisyfox <timemanager.rick@gmail.com>
2016-10-28 21:00:10 +1100

    A little bit restful api by avoiding verbs in url

--- ET_Cust/urls.py
+++ ET_Cust/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
             url(r"^$", views.CustomerRestaurantMenuView.as_view(), name='cust_restaurant_menu'),
             url(r"^checkout/$", views.CustomerRestaurantCheckOutView.as_view(), name='cust_restaurant_checkout'),
         ])),
+        url(r'^count/$', views.count_current_group, name='count_current_group'),
     ])),
     url(r"^wallet/$", views.CustomerWalletView.as_view(), name='cust_wallet'),
     url(r"^order/$", views.CustomerOrderView.as_view(), name='cust_order'),
@@ -22,9 +23,8 @@ urlpatterns = [
     url(r'^paypal/', include('paypal.standard.ipn.urls')),
 
     # URL used to handle AJAX
-    url(r"^count_people/(?P<group_id>[0-9]+)/$", views.count_people, name='count_people'),
+    url(r"^group/(?P<group_id>[0-9]+)/count$", views.count_people, name='count_people'),
     url(r"^add_favorite/(?P<restaurant_id>[0-9]+)/$", views.AddFavoriteView.as_view(), name='add_favorite'),
     url(r"^delete_favorite/(?P<restaurant_id>[0-9]+)/$", views.DeleteFavoriteView.as_view(), name='delete_favorite'),
-    url(r"^count_current_restaurant/(?P<restaurant_id>[0-9]+)$", views.count_current_group, name='count_current_group'),
     url(r"^logout/", TemplateView.as_view(template_name="ET_Cust/customer_logout.html"), name="cust_logout"),
 ]
